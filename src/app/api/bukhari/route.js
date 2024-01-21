1208c60c2c36cc2627ab3d3bc8662c606167bb41
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-21 22:47:33 +0500

    sdfnsd

--- src/app/api/bukhari/route.js
+++ src/app/api/bukhari/route.js
@@ -2,21 +2,34 @@ import { Bukhari } from "@/lib/schema/bukhari";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-let bukhariget = []
-try {
-    await mongoose.connect('mongodb+srv://abdullahhamid381:abdullahhamid786@cluster0.siyjtub.mongodb.net/BukhariDB?retryWrites=true&w=majority')
-bukhariget = await Bukhari.find()
-} catch (error) {
-    bukhariget={success:false}
+export async function GET() {
+    let bukhariget = []
+    try {
+        await mongoose.connect('mongodb+srv://abdullahhamid381:abdullahhamid786@cluster0.siyjtub.mongodb.net/BukhariDB?retryWrites=true&w=majority')
+        bukhariget = await Bukhari.find()
+    } catch (error) {
+        bukhariget = { success: false }
+    }
+
+    return NextResponse.json({ bukhariget, success: true })
 }
 
-    return NextResponse.json({bukhariget,success:true})
-}
 
-
-export async function POST(){
-    let addbukhari = new Bukhari();
+export async function POST() {
+    let addbukhari = new Bukhari(
+        {
+            Baab:
+                "A statement of revelation",
+            Book:
+                "Sahih Bukhari",
+            Chapter:
+                "The Book Of Revelation",
+            Status:
+                "Correct",
+            Hadith:
+                "2",
+        }
+    );
     const resultadd = addbukhari.find();
-    ret
+    return NextResponse.json({ resultadd, success: true })
 }
