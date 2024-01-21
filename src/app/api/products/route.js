0769e9c00f9603942c7ffd009ff408c370589ad8
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-21 21:15:36 +0500

    sdfngdfm

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(){
+
+     await mongoose.connect('')
     return NextResponse.json({result:true})
 }
 
