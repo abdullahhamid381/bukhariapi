ef5d7debd890a08b1140cd44dfa9ae1066a9da20
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-21 21:23:00 +0500

    skdmfsm,df

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,17 +1,27 @@
+import { connectionsrt } from "@/lib/db";
 import { Product } from "@/lib/schema/product";
+
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(){
+    // mongoose.set("strictQuery", true);
+    // mongoose.connect('mongodb+srv://abdullahhamid381:abdullahhamid786@cluster0.siyjtub.mongodb.net/ProductDb?retryWrites=true&w=majority', {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //   })
+    //   .then(() => {
+    //     console.log("Successfully connected to the Database");
+    //     let data = Product.find()
+    //   })
+    //   .catch((err) => console.log(err));
 
-   let data= []
+    let data=[]
    try {
-    await mongoose.connect('mongodb+srv://abdullahhamid381:abdullahhamid786@cluster0.siyjtub.mongodb.net/?retryWrites=true&w=majority');
-    data = await Product.found()
+    await mongoose.connect('mongodb+srv://abdullahhamid381:abdullahhamid786@cluster0.siyjtub.mongodb.net/ProductDb?retryWrites=true&w=majority');
+    data = await Product.find();
    } catch (error) {
     data={success:false}
    }
     return NextResponse.json({result:data})
 }
-
-
