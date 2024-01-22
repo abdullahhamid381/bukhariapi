04e0274414ca3ed134ae286fd52bd5bf6e8aa333
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-22 20:49:12 +0500

    sdnmfn

--- src/app/bukharimethood/page.js
+++ src/app/bukharimethood/page.js
@@ -3,8 +3,9 @@ import Link from "next/link"
 
 const Bukharimethood=()=>{
     return(
-        <div>
-        <Link href='bukharimethood/bukharipost'>BUkhari ADD</Link>
+        <div style={{display:'flex',justifyContent:'center',gap:'20px',marginTop:'20px'}}>
+        <Link href='bukharimethood/bukharipost' style={{textDecoration:'none'}}>BUkhari ADD</Link>
+        <Link href='bukharimethood/bukhariget' style={{textDecoration:'none'}}>BUkhari All hadith</Link>
      
         </div>
     )
