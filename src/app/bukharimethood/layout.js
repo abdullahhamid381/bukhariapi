5501bed0f5b6c3382dffff6aaf12998f289c063b
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-26 15:32:40 +0500

    sdadasdsdsdf

--- src/app/bukharimethood/layout.js
+++ src/app/bukharimethood/layout.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 const layout=({children})=>{
     return(
         <div style={{}}>
-        <Link href='/bukharipost' style={{textDecoration:'none',margin:'0 20px'}}>BUkhari ADD</Link>
+        <Link href='bukharimethood/bukharipost' style={{textDecoration:'none',margin:'0 20px'}}>BUkhari ADD</Link>
         <Link href='/bukharimethood/bukhariget'  style={{textDecoration:'none',margin:'0 20px'}}>BUkhari All hadith</Link>
      {children}
         </div>
